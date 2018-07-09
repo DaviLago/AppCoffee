9a6ed8a98f88fae71b8a8d324f962794b6c68ba4
DaviLago <davilagoo@gmail.com>
2018-07-09 20:38:05 -0300

    Diario page

---
+++ src/pages/diario/diario.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-diario',
+  templateUrl: 'diario.html'
+})
+export class DiarioPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+  }
+}
